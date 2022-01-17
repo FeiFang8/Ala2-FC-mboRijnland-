653b2446abae544c78c932465cef7ccaade1f390
FeiFang8 <82574239+FeiFang8@users.noreply.github.com>
2022-01-17 22:20:17 +0100

    ..
    ..

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {TeamsComponent} from './teams/teams.component';
+import {SpelersComponent} from './spelers/spelers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "teams",
+    component: TeamsComponent
+  },
+  {
+    path: "spelers",
+    component: SpelersComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavComponent } from './nav/nav.component';
+import { TeamsComponent } from './teams/teams.component';
+import { SpelersComponent } from './spelers/spelers.component';
+import { TeamComponent } from './team/team.component';
+import { SpelerComponent } from './speler/speler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavComponent,
+    TeamsComponent,
+    SpelersComponent,
+    TeamComponent,
+    SpelerComponent
   ],
   imports: [
     BrowserModule,
